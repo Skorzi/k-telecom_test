f81a6b9778abef211a02701a37607b40cab48378
Skorzi <lobarev_danil@mail.ru>
2023-05-18 23:56:12 +0500

    Небольшое обозначение того, что будет в БД

--- telecom_web_api/models.py
+++ telecom_web_api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Equipment(models.Model):
+    name = models.CharField(verbose_name='Наименование типа',
+                            max_length=64, blank=True)
+    sn_number = models.CharField(verbose_name='Серийный номер', max_length=10,
+                                blank=False)
+
+class Type_Of_Equipment(models.Model):
+    name = models.CharField(verbose_name='Наименование оборудования', 
+                            max_length=64, blank=True, default='Без названия')
+    sn_mask = models.CharField(verbose_name='Маска серийного номера', 
+                               blank=False, max_length=10)
