4739f4527a082775d872a3a4e3e1f2065d150061
Skorzi <lobarev_danil@mail.ru>
2023-05-19 14:03:38 +0500

    Создал метод получения всего оборудования и создание оборудования

--- telecom_web_api/serializers.py
+++ telecom_web_api/serializers.py
@@ -1,1 +1,7 @@
 from rest_framework import serializers
+from .models import Equipment, Type_Of_Equipment
+
+class EquipmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Equipment
+        fields = '__all__'
--- telecom_web_api/views.py
+++ telecom_web_api/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .serializers import EquipmentSerializer
+from .models import Equipment, Type_Of_Equipment
 # Create your views here.
 class GetOrCreateEquip(APIView):
+    def get_object(self, request, pk):
+        pass
+
     def get(self, request):
-        return Response({"message": "Hello, world!"})
+        equip = Equipment.objects.all()
+        serializer = EquipmentSerializer(equip, many=True)
+        return Response(serializer.data)
     
     def post(self, request):
         #create a new equip
-        pass
+        serializer = EquipmentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
 
     def put(self, request):
         #на редактирование записи по id
